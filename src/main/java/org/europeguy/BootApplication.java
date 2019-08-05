4a93e5d0e8bc34ca524e09e367da402c689eab58
qizhihang2017 <402185443@qq.com>
2019-08-05 14:27:25 +0800

    project init

---
+++ src/main/java/org/europeguy/BootApplication.java
@@ -0,0 +1,15 @@
+package org.europeguy;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@MapperScan("org.europeguy.mapper")
+@ComponentScan(basePackages = {"com.europeguy"})
+public class BootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+}
